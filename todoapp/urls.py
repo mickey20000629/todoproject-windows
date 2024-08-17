8ef6b48703fed3338875495f792514f1e1093efd
Mikiya Katagiri <mikiya.katagiri@gmail.com>
2024-08-18 01:30:19 +0900

    [Add]: 動的なURLでの遷移実装

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,8 @@
 from . import views
+from .views import TaskList, TaskDetail
 from django.urls import path
 
 urlpatterns = [
-    path("", views.taskList)
+    path("", TaskList.as_view()),
+    path("task/<int:pk>/", TaskDetail.as_view(), name='task')
 ]
